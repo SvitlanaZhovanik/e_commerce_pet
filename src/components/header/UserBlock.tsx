57cedd4bbfa4e8e41c50a136a72455bd71306d0c
Svitlana Zhovanik <lanasvetcat@gmail.com>
2026-04-05 14:57:27 +0300

    fix userBlock

--- src/components/header/UserBlock.tsx
+++ src/components/header/UserBlock.tsx
@@ -3,7 +3,7 @@ import TopMenu from './TopMenu';
 
 const UserBlock = () => {
   return (
-    <div className='fixed right-0 bottom-0 left-0 z-50 flex h-14 w-full flex-row items-center justify-center gap-8 bg-(--color-surface) px-4 py-2 text-[8px] shadow-(--shadow-default) md:static md:z-0 md:h-auto md:text-[12px] md:shadow-none'>
+    <div className='fixed right-0 bottom-0 left-0 z-50 flex h-14 w-full flex-row items-center justify-center gap-8 bg-(--color-surface) px-4 py-2 text-[8px] shadow-(--shadow-default) md:static md:z-0 md:h-auto md:justify-end md:gap-x-10 md:text-[12px] md:shadow-none xl:gap-x-20'>
       <TopMenu />
       <Profile />
     </div>
